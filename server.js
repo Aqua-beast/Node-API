cdf2079ca45cb2a1ca58ba6d870c8d848699e913
Aryan Sadh <85673066+Aqua-beast@users.noreply.github.com>
2023-04-24 00:19:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const Product = require('./models/productModel')
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+// this means that we can access all of our form parameters from our article form in our article route
 
 //routes
 
@@ -16,78 +17,78 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Devtamin')
 })
 
-app.get('/products', async(req, res) => {
+app.get('/products', async (req, res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-app.get('/products/:id', async(req, res) =>{
+app.get('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
-
-app.post('/products', async(req, res) => {
+app.post('/products', async (req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product);
-        
+
     } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // update a product
-app.put('/products/:id', async(req, res) => {
+app.put('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
         // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!product) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // delete a product
-
-app.delete('/products/:id', async(req, res) =>{
+app.delete('/products/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        if (!product) {
+            return res.status(404).json({ message: `cannot find any product with ID ${id}` })
         }
         res.status(200).json(product);
-        
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 mongoose.set("strictQuery", false)
 mongoose.
-connect('mongodb+srv://admin:12345678Admin@devtaminapi.zpncstm.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(() => {
-    console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000`)
-    });
-}).catch((error) => {
-    console.log(error)
-})
+    connect(process.env.MONGODB_URI)
+
+    .then(() => {
+        // then here is a promise
+        console.log('connected to MongoDB')
+        app.listen(process.env.PORT, () => {
+            console.log(`Node API app is running on port ${process.env.PORT}`)
+        });
+    }).catch((error) => {
+        console.log(error)
+    })
