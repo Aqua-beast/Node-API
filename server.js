d567f65766fd4a01e2f5b5a571885f830f30fcb8
Aryan Sadh <85673066+Aqua-beast@users.noreply.github.com>
2023-04-24 00:51:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -79,11 +79,11 @@ app.delete('/products/:id', async (req, res) => {
     }
 })
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fzv6xup.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority"
+
 
 mongoose.set("strictQuery", false)
 mongoose.
-    connect(uri)
+    connect(process.env.MONGODB_URI)
 
     .then(() => {
         // then here is a promise
